20cddc0bb1edaa123a87ab4fc62563ca06534027
ParkSeonGyun <qkrtjsrbs315@gmail.com>
2023-08-02 22:57:25 +0900

    Feat : entity class

--- src/main/java/com/likelion/loco/entities/Post.java
+++ src/main/java/com/likelion/loco/entities/Post.java
@@ -3,7 +3,7 @@ package com.likelion.loco.entities;
 import com.likelion.loco.global.BaseEntity;
 import lombok.*;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -12,4 +12,14 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Post extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postIdx;
+
+    @Column(name = "post_title")
+    private String postTitle;
+
+    @Column(name = "post_content")
+    private String postContent;
+
 }
--- src/main/java/com/likelion/loco/entities/Review.java
+++ src/main/java/com/likelion/loco/entities/Review.java
@@ -3,7 +3,7 @@ package com.likelion.loco.entities;
 import com.likelion.loco.global.BaseEntity;
 import lombok.*;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -12,6 +12,20 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewIdx;
+
+    @Column(name="review_content")
+    private String reviewContent;
+
+    @Column(name="review_star")
+    private Integer reviewStar;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
 
 
 }
