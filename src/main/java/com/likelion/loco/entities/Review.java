bf8a7d1c57a8271cba7f4edbd05665f785739ce6
khw18 <gmsb14@naver.com>
2023-08-02 20:24:26 +0900

    Feat: 엔티티 추가

---
+++ src/main/java/com/likelion/loco/entities/Review.java
@@ -0,0 +1,15 @@
+package com.likelion.loco.entities;
+
+import com.likelion.loco.global.BaseEntity;
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review extends BaseEntity {
+}
