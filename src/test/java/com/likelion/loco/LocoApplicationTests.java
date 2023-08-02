fe3277a3a9de7c2eafc687f6037385d709c7b5c4
khw18 <gmsb14@naver.com>
2023-08-02 20:11:28 +0900

    Feat: 프로젝트 프레임

---
+++ src/test/java/com/likelion/loco/LocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.loco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
